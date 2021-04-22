2a9dfa938b11b23da3aed7f50895dcd044ffa960
Jiapeng Guo <jiapeng.1.guo@gmail.com>
2021-04-21 22:26:10 -0400

    path sum

---
+++ binary tree/pathsum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def pathSum(self , root , sum ):
+        if not root:
+            return []
+        res = []
+        stack = [(root, sum-root.val, [root.val])]
+        while stack:
+            curr, val, ls = stack.pop()
+            if not curr.left and not curr.right and val == 0:
+                res.append(ls)
+            if curr.right:
+                stack.append((curr.right, val-curr.right.val, ls+[curr.right.val]))
+            if curr.left:
+                stack.append((curr.left, val-curr.left.val, ls+[curr.left.val]))
+        return res
+
+def hasPathSum1(self, root, sum):
+    res = []
+    self.dfs(root, sum, res)
+    return any(res)
+
+def dfs(self, root, target, res):
+    if root:
+        if not root.left and not root.right and root.val == target:
+            res.append(True)
+        if root.left:
+            self.dfs(root.left, target-root.val, res)
+        if root.right:
+            self.dfs(root.right, target-root.val, res)
