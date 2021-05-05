2aae75910c6ca0136b50a16714d3a31d62433e88
Jiapeng Guo <jiapeng.1.guo@gmail.com>
2021-05-05 12:26:04 -0400

    add comment

--- sliding_window.py
+++ sliding_window.py
@@ -4,9 +4,12 @@ def sliding(arr, size):
     dq = deque()
     ans = []
     for i in range(len(arr)):
+        # if new element is larger than right most ele, pop it
         while dq and arr[i] >= arr[dq[-1]]:
             dq.pop()
+        # maintain a stricly decreasing deque from left to right
         dq.append(i)
+        # pop old ele to maintain proper size
         if i - size >= dq[0]:
             dq.popleft()
         if i + 1 - size >= 0:
