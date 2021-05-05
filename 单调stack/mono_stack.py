30634d3c40eaabf58b2bd3dd1fe53ea992680bbd
Jiapeng Guo <jiapeng.1.guo@gmail.com>
2021-05-05 12:52:22 -0400

    739. Daily Temperatures

---
+++ 单调stack/mono_stack.py
@@ -0,0 +1,14 @@
+''' 739. Daily Temperatures '''
+def rightMostCloseMin(arr):
+    ''' find the right cloest value that is smaller that each element '''
+    # a stack to to record the position, stricly increasing order
+    stack = []
+    ans = [0] * len(arr)
+    for i in range(len(arr)):
+        while stack and arr[i] > arr[stack[-1]]:
+            pop_index = stack.pop()
+            ans[pop_index] = i - pop_index
+        stack.append(i)
+    return ans
+
+print(rightMostCloseMin([73, 74, 75, 71, 69, 72, 76, 73]))
