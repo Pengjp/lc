c638edc1eed4213ed509e6a0de1dfbf25f94beb5
Jiapeng Guo <jiapeng.1.guo@gmail.com>
2021-05-05 19:29:40 -0400

    add commment

--- binary tree/morris.py
+++ binary tree/morris.py
@@ -24,12 +24,12 @@ def preorder(root):
             while mostRight.right and mostRight.right != cur:
                 mostRight = mostRight.right
             # 跳出while并且右节点为空，根据规则，让右节点指向cur，cur向左移动
-            if not mostRight.right:
+            if not mostRight.right: # 第一次访问
                 ans.append(cur.val)
                 mostRight.right = cur
                 cur = cur.left
                 continue
-            else:
+            else: # 意味着第二次访问节点
                 mostRight.right = None
         else:
             ans.append(cur.val)
