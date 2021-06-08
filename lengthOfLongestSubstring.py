d7591b6c8f922a2785d4f5988b80f1d05e65ab2e
Jiapeng Guo <jiapeng.1.guo@gmail.com>
2021-06-08 07:50:28 -0400

    add comment

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,7 +1,10 @@
+''' 最长无重复子序列 '''
+'''' Longest Substring Without Repeating Characters '''
 def lengthOfLongestSubstring(arr):
     start, ans = 0, 0
     seen = {}
     for index, ele in enumerate(arr):
+        # 如果当前位置见到过，并且子序列的开始的位置是小于等于这个已经见过的元素的位置，更新
         if ele in seen and start <= seen[ele]:
             start = seen[ele] + 1
         else:
@@ -9,4 +12,5 @@ def lengthOfLongestSubstring(arr):
         seen[ele] = index
     return ans
 
-print(lengthOfLongestSubstring([2,2,3,4,3]))
+print(lengthOfLongestSubstring([2,2,3,4,5]))
+print(lengthOfLongestSubstring([2,1,3,3,4,5,6,7]))
